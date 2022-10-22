aa769b5994fac68eabdaba651fad82025fdff4a7
Ana Helena <anahelenafaustinoteodoro@gmail.com>
2022-10-22 15:42:39 -0300

    🪄if da maldade

---
+++ Basico/if.py
@@ -0,0 +1,9 @@
+idade = int(int(input('digite a sua idade: ')))
+if idade < 12:
+    print('crianca')
+elif idade < 18:
+    print('adolescente')
+elif idade < 60:
+    print('adulto')
+else:
+    print('idoso')
